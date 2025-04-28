18f5ac8a16395dc147e71540dd0fc5894b51c370
MohamadAbudraiea <mohamadabudraiea2003@outlook.com>
2025-04-28 20:32:47 +0300

    Adding new repo

--- log_db/db.js
+++ log_db/db.js
@@ -1,17 +1,20 @@
 // db.js
 import pg from "pg";
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets HTC",
-  password: "4412106",
-  port: 5432,
-});
+const db = new pg.Client(
+  "postgresql://postgres.dctbfwejkssojlvkcpol:4412106@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
+);
 
-await db.connect(); // You can also handle errors here if needed
-console.log("✅ Connected to PostgreSQL");
-
+await db
+  .connect()
+  .then(() => {
+    console.log(
+      "✅ the Connection has been established successfully to SUPBASE"
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  }); // You can also handle errors here if needed
 export default db;
